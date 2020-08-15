0946d8b132c58cfeb9a803f0a551379d9fee53b0
smardone <shaun.mardones@gmail.com>
2020-08-14 22:38:25 -0400

    added zookeeper functionality

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'))
+});
+
+
+
+module.exports = router;
